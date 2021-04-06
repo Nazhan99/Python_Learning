04cd3d38a2ceed48b2674d0028d8cfca5b33c5ec
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2021-04-06 15:55:41 +0800

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,8 @@
+course='Python for Beginners'
+print(course.upper()) #print uppercase
+print(course.find('y'))
+print(course)
+
+print(course.replace('for', '4')) #replace word with new word
+
+print('Python' in course) #find keyword with boolean output
---
+++ app3.py
@@ -0,0 +1,10 @@
+print(10 / 3) #floating result
+print(10//3) #whole numebr
+print(10**3) #10 power of 3
+
+x=10
+x = x+3
+x += 3
+
+print(x)
+

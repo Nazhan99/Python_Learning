354e1e6b26a8374770fab0f70ac2cea784205faa
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2021-04-06 20:38:04 +0800

    Add files via upload
    if condition

---
+++ app4.py
@@ -0,0 +1,13 @@
+temperature= 10
+
+if temperature>30:
+    print('Hot Day')
+elif temperature >20: #20 to 30
+    print("It's a nice day")
+elif temperature>10: #10 to 20
+    print("It's pretty cold")
+else:
+    print("It's cold")
+
+
+
